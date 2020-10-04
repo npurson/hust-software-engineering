0c779f2d7a6131553d9716be1cc570454b3e459d
Natter Purson <natterpurson@qq.com>
2020-10-04 11:59:54 +0800

    Feat: init_map

--- map.cpp
+++ map.cpp
@@ -5,63 +5,67 @@ map_t init_map()
 {
     map_t map;
     for (int i = 0; i < 70; ++i) {
-        // 地段1
-        if (i >= 1 && i <=27 && i != 14) {
-
+        switch (i) {
+            case 0: map.emplace_back(START); break;
+            case 14: map.emplace_back(HOSPITAL); break;
+            case 28: map.emplace_back(ITEM_HOUSE); break;
+            case 35: map.emplace_back(GIFT_HOUSE); break;
+            case 49: map.emplace_back(PRISON); break;
+            case 63: map.emplace_back(MAGIC_HOUSE); break;
+            case 64: map.emplace_back(MINE, 60); break;
+            case 65: map.emplace_back(MINE, 80); break;
+            case 66: map.emplace_back(MINE, 40); break;
+            case 67: map.emplace_back(MINE, 100); break;
+            case 68: map.emplace_back(MINE, 20); break;
+            default:
+                // 地段1
+                if (i >= 1 && i <=27 && i != 14)
+                    map.emplace_back(VACANCY, 200);
+                // 地段2
+                else if (i >= 29 && i <=34)
+                    map.emplace_back(VACANCY, 500);
+                    // 地段3
+                else if (i >= 36 && i <= 62 && i != 49)
+                    map.emplace_back(VACANCY, 300);
         }
-        // 地段2
-        else if (i >= 29 && i <=34) {
-
-        }
-        // 地段3
-        else if (i >= 36 && i <= 62 && i != 49) {
-
-        }
-        // 起点
-        else if (i == 0) {
-
-        }
-        else if (i == 14) {
-
-        }
-        else if (i == )
     }
+    return map;
 }
 
 
-void plot_map()
-{
-
-}
-
-
-void buy_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx)
-{
-
-}
-
-
-void update_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx)
-{
-
-}
-
-
-void sell_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx)
-{
-
-}
-
-
-void apply_item(map_t& map, uint8_t item_type, uint8_t pos)
-{
-
-}
-
-
-void roll_dice(map_t& map, player_t& player)
-{
-    for (int dice = rand() % max + 1; dice; --dice) {
-
-    }
-}
+//void plot_map()
+//{
+//
+//}
+//
+//
+//void buy_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx)
+//{
+//
+//}
+//
+//
+//void update_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx)
+//{
+//
+//}
+//
+//
+//void sell_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx)
+//{
+//
+//}
+//
+//
+//void apply_item(map_t& map, uint8_t item_type, uint8_t pos)
+//{
+//
+//}
+//
+//
+//void roll_dice(map_t& map, player_t& player)
+//{
+//    for (int dice = rand() % max + 1; dice; --dice) {
+//
+//    }
+//}
--- map.h
+++ map.h
@@ -9,7 +9,8 @@
 #include <windows.h>
 
 using std::vector;
-using std::uint8_t, std::uint16_t;
+using std::uint8_t;
+using std::uint16_t;
 
 
 // 地图结点类型
@@ -29,6 +30,7 @@ typedef struct map_node {
     uint8_t item;               // 当前置于该结点的道具。枚举类型为ItemType或None
     uint16_t value;             // type==VACANCY时为空地价格，计算房产价值时乘以（房产等级+1）；type==MINE时为可获取的点数
 
+    map_node(uint8_t t) :type(t), estate_lvl(), owner(), players(), item(), value() {}
     map_node(uint8_t t, uint16_t v) :type(t), value(v), estate_lvl(), owner(), players(), item() {}
 } map_node_t, * p_map_node_t;
 
@@ -37,19 +39,19 @@ typedef vector<map_node_t> map_t, * p_map_t;
 
 // 初始化地图
 map_t init_map();
-// 读取地图
-p_map_t get_map();
-// 绘制地图
-void plot_map();
-// 购买房产
-void buy_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx);
-// 升级房产
-void update_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx);
-// 卖出房产
-void sell_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx);
-// 使用道具
-void apply_item(map_t& map, uint8_t item_type, uint8_t pos);
-// 投骰子，行走过程中判定道具、地段、破产  TODO 破产处理应在主调函数中？
-void roll_dice(map_t& map, player_t& player);
+//// 读取地图
+//p_map_t get_map();
+//// 绘制地图
+//void plot_map();
+//// 购买房产
+//void buy_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx);
+//// 升级房产
+//void update_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx);
+//// 卖出房产
+//void sell_estate(map_t& map, uint8_t player_idx, uint8_t map_node_idx);
+//// 使用道具
+//void apply_item(map_t& map, uint8_t item_type, uint8_t pos);
+//// 投骰子，行走过程中判定道具、地段、破产  TODO 破产处理应在主调函数中？
+//void roll_dice(map_t& map, player_t& player);
 
 #endif // RICH_MAP_H
