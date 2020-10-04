b8d5b47bc73c198359270ff7bbfdaf3239087477
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-10-04 18:13:26 +0800

    parse

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ extern std::uint64_t init_money;
 int main() {
     srand(time(0));
     p_map_t map = init_map();
-//    plot_map();
+    plot_map();
     std::string command;
     init_money = 10000;
     add_player('Q');
@@ -17,6 +17,7 @@ int main() {
     while (true) {
         show_cmd();
         if (std::getline(std::cin, command)) {
+            plot_map();
             if (command == "quit") {
                 break;
             }
